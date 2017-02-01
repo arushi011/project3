c187ff49bc55b0915f707efc0f2bf8fcc2781c79
Arushi Bajpai <arushi000002@gmail.com>
2017-02-01 21:49:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 'use strict';
 app.controller('HomeCtrl',
-        ["$sce", "$timeout", "$http", function ($sce, $timeout, $http) {
+        ["$sce", "$timeout","$http", function ($sce, $timeout, $http) {
             var controller = this;
             controller.state = null;
             controller.API = null;
             controller.currentVideo = 0;
-			controller.videos = [[],[]];
 
             controller.onPlayerReady = function(API) {
                 controller.API = API;
@@ -21,41 +20,22 @@ app.controller('HomeCtrl',
                 controller.setVideo(controller.currentVideo);
             };
 
-		 	$http.get("current.json").then(function (response) {
+		 	controller.videos = [];
 			
-		
-			for (var i=0, l=response.data.length; i<l; i++) {
- 
-                    for (var mi=0, ml=response.data[i].length; mi<ml; mi++) {
-                        
-						var mediaFile = {
-                        src: $sce.trustAsResourceUrl(response.data[i][mi].src), type: response.data[i][mi].type
-						};
-						
-						controller.videos[i].push(mediaFile);
-					
-					}
-                }
-				
-			});
-		
 			
-            /*controller.videos = [
-            {
-                sources: [
-                    {src: ("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-                    {src: ("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-                    {src: ("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-                ]
-            },
-            {
-                sources: [
-                    {src: ("http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov"), type: "video/mp4"},
-                    {src: ("http://static.videogular.com/assets/videos/big_buck_bunny_720p_stereo.ogg"), type: "video/ogg"}
-                ]
-            }
-				
-        ];*/
+			$http.get("current.json").then(function (response) {
+			
+			for (var i=0, l=response.data.sources.length; i<l; i++)
+			{
+			console.log(response.data.sources[i]);
+			controller.videos.push(response.data.sources[i]);
+			}
+			console.log("all list loaded");
+			console.log(response.data);
+			console.log(controller.videos);
+			
+		});
+		
 
             controller.config = {
                 preload: "none",
@@ -72,6 +52,7 @@ app.controller('HomeCtrl',
             };
 
             controller.setVideo = function(index) {
+				controller.isCompleted = true;
                 controller.API.stop();
                 controller.currentVideo = index;
                 controller.config.sources = controller.videos[index];
